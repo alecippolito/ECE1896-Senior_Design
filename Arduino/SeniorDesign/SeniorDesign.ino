d3f4563661bf0a88fd3ab5059419c9176c2cc167
Thomas Cheezum <techeezum@gmail.com>
2023-02-14 23:58:20 -0500

    Add functional serial interpretation

---
+++ Arduino/SeniorDesign/SeniorDesign.ino
@@ -0,0 +1,47 @@
+
+int led = 8;
+int incoming = 0;
+int delayTime = 1000;
+
+void setup()
+{
+  pinMode(led, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  if(Serial.available() > 0)
+  {
+    //Serial.write(Serial.read());
+    blinkByte(Serial.read());
+
+    Serial.flush();
+  }
+}
+
+void blinkByte(int byteRead)
+{
+  for(int i=0; i<8; i++)
+  {
+    if(bitRead(byteRead, i) == 1)
+    {
+      digitalWrite(led, HIGH);
+      delay(delayTime / 2);
+      digitalWrite(led, LOW);
+      delay(delayTime / 2);
+      digitalWrite(led, HIGH);
+      delay(delayTime / 2);
+      digitalWrite(led, LOW);
+      delay(delayTime / 2);
+    }
+    else
+    {
+      digitalWrite(led, HIGH);
+      delay(delayTime);
+      digitalWrite(led, LOW);
+      delay(delayTime);
+    }
+  }
+}
