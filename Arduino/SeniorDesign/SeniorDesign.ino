8fe7a9c34d572fec30721dcbc8fdc7031ba95523
Thomas Cheezum <techeezum@gmail.com>
2023-03-20 18:15:50 -0400

    Rethink transmission and reception so they can be operated in the same interrupt cycle

--- Arduino/SeniorDesign/SeniorDesign.ino
+++ Arduino/SeniorDesign/SeniorDesign.ino
@@ -7,47 +7,43 @@
 #include <TimerInterrupt.h>
 #include <TimerOne.h>
 
-int led = 8, button = 9;
-int delayTime = 500;
+int led = 8, receiverIn = A0, transmitterOut = 10;
+uint8_t startByte = 1, endByte;
+int outputTest = 9;
 
-int in = A0;
-int out = 10;
-int interruptPin = 2;
-uint8_t incoming = 0;
-uint8_t receivedBits = 0;
-     
+int delayTime = 250;
+bool delayEnable = false, startFlagOverride = false;
+
+uint8_t incoming = 0, outgoing = 0, transmittedBits = 0, receivedBits = 0;
+bool transmitFlag = false, receiveFlag = true, oFlag = false, startFlag = false;
 bool o = false;
 
-//int highFreq = 41; // 50KHz square wave from timer 1
-int highFreq = 20; // 50KHz square wave from timer 1
-//int lowFreq = 62; // 25KHz square wave from timer 1
-int lowFreq = 40; // 25KHz square wave from timer 1
-int standbyFreq = 100;
+// Hardware Timer 1 frequency values:
+int highFreq = 41; // 24KHz square wave
+//int highFreq = 20; // 50KHz square wave
+int lowFreq = 62; // 16KHz square wave
+//int lowFreq = 40; // 25KHz square wave
+int standbyFreq = 100; // 10KHz square wave
 
-int timer2Freq = 1000;
-
-byte data = 0;
+// Hardware Timer 2 frequency values:
+int interruptFreq = 1;
 
 void setup()
-{  
+{
   pinMode(led, OUTPUT);
-  pinMode(button, INPUT);
+  pinMode(receiverIn, INPUT);
+  pinMode(outputTest, OUTPUT);
 
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), interruptReceive, RISING); 
-
-  pinMode(in, INPUT);
-  
   // Setup PWM pin based on 1MHz timer1 for transmitter:
-  pinMode(out, OUTPUT);
+  pinMode(transmitterOut, OUTPUT);
   Timer1.initialize(standbyFreq);
-  Timer1.pwm(out, 512);
+  Timer1.pwm(transmitterOut, 512);
   //Timer1.stop();
 
   // Setup Timer2 for transmit/receive operations:
   ITimer2.init();
-  //ITimer2.attachInterrupt(timer2Freq, timerHandler);
-  //ITimer2.attachInterrupt(timer2Freq, interruptReceive);
+  //ITimer2.attachInterrupt(interruptFreq, timerHandler);
+  ITimer2.attachInterrupt(interruptFreq, interruptHandler);
 
   Serial.begin(9600);
 }
@@ -66,9 +62,27 @@ void loop()
     incoming = Serial.read();
     //Serial.write(incoming);
     //blinkByte(incoming);
-    pwmByte(incoming);
+    //pwmByte(incoming);
   }
 
+  if (transmitFlag)
+  {
+    transmitBit();
+    transmitFlag = false;
+  }
+
+  if (receiveFlag)
+  {
+    receiveBit();
+    receiveFlag = false;
+  }
+
+  /*if(oFlag)
+    {
+    digitalWrite(led, o);
+    o = !o;
+    oFlag = false;
+    }*/
 }
 
 void blinkByte(uint8_t byteRead)
@@ -83,23 +97,21 @@ void blinkByte(uint8_t byteRead)
     if (bitRead(byteRead, i) == 1)
     {
       digitalWrite(led, HIGH);
-      delay(delayTime / 2);
+      doDelay();
       digitalWrite(led, LOW);
-      delay(delayTime / 2);
+      doDelay();
       digitalWrite(led, HIGH);
-      delay(delayTime / 2);
+      doDelay();
       digitalWrite(led, LOW);
-      delay(delayTime / 2);
+      doDelay();
     }
     else
     {
       digitalWrite(led, HIGH);
-      delay(delayTime);
+      doDelay();
       digitalWrite(led, LOW);
-      delay(delayTime);
+      doDelay();
     }
-
-    delay(delayTime / 2);
   }
 }
 
@@ -115,60 +127,147 @@ void pwmByte(uint8_t byteRead)
     if (bitRead(byteRead, i) == 1)
     {
       Timer1.setPeriod(highFreq);
-      Timer1.setPwmDuty(out, 512);
-      
-      //Timer1.start();
-      delay(delayTime);
-      //Timer1.stop();
-      delay(delayTime);
+      Timer1.setPwmDuty(transmitterOut, 512);
+
+      doDelay();
     }
     else
     {
       Timer1.setPeriod(lowFreq);
-      Timer1.setPwmDuty(out, 512);
+      Timer1.setPwmDuty(transmitterOut, 512);
 
-      //Timer1.start();
-      delay(delayTime);
-      //Timer1.stop();
-      delay(delayTime);
+      doDelay();
     }
 
     digitalWrite(led, HIGH);
-    delay(delayTime / 2);
+    doDelay();
     digitalWrite(led, LOW);
-    delay(delayTime / 2);
+    doDelay();
   }
 
   Timer1.setPeriod(standbyFreq);
-  Timer1.setPwmDuty(out, 512);
+  Timer1.setPwmDuty(transmitterOut, 512);
 }
 
-void interruptReceive()
+void doDelay()
 {
-  byte holder;
-  
-  if(analogRead(in) > 512)
+  if (delayEnable)
   {
-    //digitalWrite(led, HIGH);
+    delay(delayTime);
+  }
+}
+
+void transmitBit()
+{
+  if (incoming == 0)
+  {
+    return;
+  }
+
+  if(incoming == startByte)
+  {
+    transmittedBits = 0;
+  }
+
+  if (bitRead(incoming, transmittedBits) == 1)
+  {
+    Timer1.setPeriod(highFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, true);
+
+    doDelay();
+  }
+  else
+  {
+    Timer1.setPeriod(lowFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, false);
+
+    doDelay();
+  }
+
+  if (delayEnable)
+  {
+    digitalWrite(led, HIGH);
+    doDelay();
+    digitalWrite(led, LOW);
+    doDelay();
+  }
+
+  Serial.write(incoming);
+
+  if (transmittedBits < 7)
+  {
+    transmittedBits++;
+  }
+  else
+  {
+    transmittedBits = 0;
+    incoming = 0;
+
+    Timer1.setPeriod(standbyFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, false);
+  }
+}
+
+void receiveBit()
+{
+  uint8_t holder;
+
+  if (analogRead(receiverIn) > 512)
+  {
     holder = 1;
   }
   else
   {
-    //digitalWrite(led, LOW);
     holder = 0;
   }
-  
-  data |= holder << receivedBits; // need this in final
-  
-  if(receivedBits < 8)
+
+  Serial.write(holder);
+
+  if(!startFlag || startFlagOverride)
   {
-    receivedBits++;
+    outgoing <<= 1;
+    outgoing += holder;
   }
   else
   {
-    receivedBits = 0;
-    Serial.write(data);
-    Serial.flush();
-    data = 0;
+    outgoing |= holder << receivedBits;
   }
+
+  Serial.write(outgoing);
+
+  if(startFlag)
+  {
+    if (receivedBits < 8) receivedBits++;
+    else
+    {
+      if(outgoing != 0)
+      {
+        Serial.write(outgoing);
+        Serial.flush();
+      }
+  
+      receivedBits = 0;
+      outgoing = 0;
+    } 
+  }
+
+  if(outgoing == startByte && !startFlag)
+  {
+    startFlag = true;
+    receivedBits = 0;
+    outgoing = 0;
+  }
+}
+
+void interruptHandler()
+{
+  transmitFlag = true;
+  receiveFlag = true;
+  oFlag = true;
 }
