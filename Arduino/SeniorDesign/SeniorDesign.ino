1d2c378d7ca75343f1e824ee0f0a6529b144c3cd
Thomas Cheezum <techeezum@gmail.com>
2023-03-27 12:00:18 -0400

    Get Receiver working properly

--- Arduino/SeniorDesign/SeniorDesign.ino
+++ Arduino/SeniorDesign/SeniorDesign.ino
@@ -8,14 +8,14 @@
 #include <TimerOne.h>
 
 int led = 8, receiverIn = A0, transmitterOut = 10;
-uint8_t startByte = 'A', endByte = 'C', charDelay = 20;
+uint8_t charDelay = 20, comma = 255;
 int outputTest = 9;
 
 int delayTime = 250;
 bool delayEnable = false;
 
 uint8_t incoming = 0, outgoing = 0, transmittedBits = 0, receivedBits = 0;
-bool transmitFlag = false, receiveFlag = false, newTransmission = false, newReception = false, startFlag = false;
+bool transmitFlag = false, receiveFlag = false, newTransmission = false, newReception = false;
 bool o = false;
 
 // Hardware Timer 1 frequency values:
@@ -26,7 +26,7 @@ int lowFreq = 62; // 16KHz square wave
 int standbyFreq = 100; // 10KHz square wave
 
 // Hardware Timer 2 frequency value:
-int interruptFreq = 10000;
+int interruptFreq = 20000;
 
 void setup()
 {
@@ -57,7 +57,7 @@ void timerHandler()
 void loop()
 {
   // Read from PC
-  if (Serial.available() > 0 && (!newTransmission || transmittedBits == 0))
+  if (Serial.available() > 0 && !newTransmission && transmittedBits == 0)
   {
     incoming = Serial.read();
     newTransmission = true;
@@ -74,6 +74,168 @@ void loop()
     receiveBit();
     receiveFlag = false;
   }//*/
+}
+
+void transmitBit()
+{
+  delayMicroseconds(charDelay);
+  if(incoming == 0)
+  {
+    return;
+  }
+
+  //Serial.write(incoming);
+
+  if(newTransmission)
+  {
+    transmittedBits = 0;
+  }
+
+  if(newTransmission || transmittedBits < 8)
+  {
+    Timer1.setPeriod(highFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, true);
+
+    doDelay();
+  }
+  else if (bitRead(incoming, transmittedBits-8) == 1)
+  {
+    Timer1.setPeriod(highFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, true);
+
+    doDelay();
+  }
+  else
+  {
+    Timer1.setPeriod(lowFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, false);
+
+    doDelay();
+  }
+
+  /*if(digitalRead(outputTest) == true) Serial.write(1);
+  else Serial.write(0);//*/
+
+  if (delayEnable)
+  {
+    digitalWrite(led, HIGH);
+    doDelay();
+    digitalWrite(led, LOW);
+    doDelay();
+  }
+
+  if (transmittedBits < 15)
+  {
+    newTransmission = false;
+    transmittedBits++;
+  }
+  else
+  {
+    //Serial.write(incoming);
+    transmittedBits = 0;
+    incoming = 0;
+
+    Timer1.setPeriod(standbyFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, false);
+  }
+}
+
+void receiveBit()
+{
+  //delayMicroseconds(charDelay);
+  uint8_t votes = 5, carry;
+  float holder = 0;
+
+  for(int i=0; i<votes; i++)
+  {
+    if (analogRead(receiverIn) > 512)
+    {
+      holder += 1;
+    }
+    else
+    {
+      holder += 0;
+    }
+  }
+
+  holder /= votes;
+
+  if(!newReception)
+  {
+    if(holder > 0.5)
+    {
+      outgoing <<= 1;
+      outgoing += 1;
+      carry = 1;
+    }
+    else
+    {
+      outgoing <<= 1;
+      outgoing += 0;
+      carry = 0;
+    } 
+  }
+  else
+  {
+    if(holder > 0.5)
+    {
+      outgoing |= 1 << receivedBits;
+      carry = 1;
+    }
+    else
+    {
+      outgoing |= 0 << receivedBits;
+      carry = 0;
+    }
+  }
+
+  //Serial.write(outgoing);
+
+  if(outgoing == comma && !newReception)
+  {
+    receivedBits = 0;
+    outgoing = 0;
+    newReception = true;
+
+    //Serial.write(238);
+  }
+  else if(newReception)
+  {
+    if (receivedBits < 7) receivedBits++;
+    else
+    {
+      if(outgoing != 0)
+      {
+        Serial.write(outgoing);
+      }
+
+      receivedBits = 0;
+      outgoing = 0;
+      newReception = false;
+    }    
+  }
+}
+
+void interruptHandler()
+{
+  transmitFlag = true;
+  receiveFlag = true;
+}
+
+void doDelay()
+{
+  if (delayEnable)
+  {
+    delay(delayTime);
+  }
 }
 
 void blinkByte(uint8_t byteRead)
@@ -138,164 +300,4 @@ void pwmByte(uint8_t byteRead)
 
   Timer1.setPeriod(standbyFreq);
   Timer1.setPwmDuty(transmitterOut, 512);
-}
-
-void doDelay()
-{
-  if (delayEnable)
-  {
-    delay(delayTime);
-  }
-}
-
-void transmitBit()
-{
-  delayMicroseconds(charDelay);
-  if(incoming == 0)
-  {
-    return;
-  }
-
-  //Serial.write(incoming);
-
-  if(newTransmission)
-  {
-    transmittedBits = 0;
-  }
-
-  if (bitRead(incoming, transmittedBits) == 1)
-  {
-    Timer1.setPeriod(highFreq);
-    Timer1.setPwmDuty(transmitterOut, 512);
-
-    digitalWrite(outputTest, true);
-
-    doDelay();
-  }
-  else
-  {
-    Timer1.setPeriod(lowFreq);
-    Timer1.setPwmDuty(transmitterOut, 512);
-
-    digitalWrite(outputTest, false);
-
-    doDelay();
-  }
-
-  if (delayEnable)
-  {
-    digitalWrite(led, HIGH);
-    doDelay();
-    digitalWrite(led, LOW);
-    doDelay();
-  }
-
-  if (transmittedBits < 7)
-  {
-    newTransmission = false;
-    transmittedBits++;
-  }
-  else
-  {
-    //Serial.write(incoming);
-    transmittedBits = 0;
-    incoming = 0;
-
-    Timer1.setPeriod(standbyFreq);
-    Timer1.setPwmDuty(transmitterOut, 512);
-
-    digitalWrite(outputTest, false);
-  }
-}
-
-void receiveBit()
-{
-  delayMicroseconds(charDelay);
-  uint8_t votes = 5, carry;
-  float holder = 0;
-
-  for(int i=0; i<votes; i++)
-  {
-    if (analogRead(receiverIn) > 512)
-    {
-      holder += 1;
-    }
-    else
-    {
-      holder += 0;
-    }
-  }
-
-  holder /= votes;
-
-  if(startFlag)
-  {
-    if(holder > 0.5) 
-    {
-      outgoing |= 1 << receivedBits;
-      carry = 1;
-    }
-    else
-    {
-      outgoing |= 0 << receivedBits;
-      carry = 0;
-    }
-  }
-  else
-  {
-    if(holder > 0.5)
-    {
-      outgoing <<= 1;
-      outgoing += 1;
-      carry = 1;
-    }
-    else
-    {
-      outgoing <<= 1;
-      outgoing += 0;
-      carry = 0;
-    }
-  }
-
-  //if(outgoing !=0 && !newReception) newReception = true;
-  //else if(outgoing == 0 && newReception) newReception = false;
-
-  if(outgoing == startByte && !startFlag)
-  {
-    startFlag = true;
-    receivedBits = 0;
-    outgoing = 0;
-  }
-  else if(outgoing == endByte && startFlag)
-  {
-    startFlag = false;
-  }
-
-  if(startFlag)
-  {
-    if(outgoing != startFlag)
-    {
-      if (receivedBits < 7) receivedBits++;
-      else
-      {
-        if(outgoing != 0)
-        {
-          Serial.write(outgoing);
-        }
-    
-        receivedBits = 0;
-        outgoing = 0;
-      }
-    }
-    else
-    {
-      Serial.write(outgoing);
-    }
-  }
-}
-
-void interruptHandler()
-{
-  transmitFlag = true;
-  receiveFlag = true;
 }
