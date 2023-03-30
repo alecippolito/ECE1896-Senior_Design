bd0a0d30c69f7e51fb1c015b438da205931704b4
Thomas Cheezum <techeezum@gmail.com>
2023-03-29 22:08:26 -0400

    Add two different functions for transmission and reception on start/stop bytes

--- Arduino/SeniorDesign/SeniorDesign.ino
+++ Arduino/SeniorDesign/SeniorDesign.ino
@@ -7,9 +7,10 @@
 #include <TimerInterrupt.h>
 #include <TimerOne.h>
 
-int led = 8, receiverIn = A0, transmitterOut = 10;
-uint8_t charDelay = 20, comma = 1, errorChance = 1;
-int outputTest = 9;
+int receiverIn = A0, transmitterOut = 10;
+uint8_t charDelay = 20, comma = 1;
+uint8_t startMessage = 1, endMessage = 4, startChunk = 0, endChunk = 0;
+int outputTest = 9, led = 8, errorChance = 100;
 
 int delayTime = 250;
 bool delayEnable = false;
@@ -63,18 +64,25 @@ void loop()
 
   if (transmitFlag)
   {
-    transmitBit();
+    //transmitBitExtraBit();
+    transmitBitStartStop();
     transmitFlag = false;
   }
 
   if (receiveFlag)
   {
-    receiveBit();
+    //receiveBitExtraBit();
+    receiveBitStartStop();
     receiveFlag = false;
   }//*/
 }
 
-void transmitBit()
+/*/  
+ *  Breaks down incoming serial bytes and transmits bits one at a time by sending them as 24kHz square wave for 1, 16kHz square wave for 0.
+ *  To differentiate bytes on the receiver end, adds one extra bit to the front of every byte.
+ *  This increases the number of bits required to transmit messages by 1.125, which is not ideal.
+/*/
+void transmitBitExtraBit()
 {
   delayMicroseconds(charDelay);
   if(incoming == 0)
@@ -144,7 +152,76 @@ void transmitBit()
   }
 }
 
-void receiveBit()
+/*/
+ *  Breaks down incoming serial bytes and transmits bits one at a time by sending them as 24kHz square wave for 1, 16kHz square wave for 0.
+ *  Differentiation between differing bytes is entirely handled by the receiving end, no additional bits/bytes are added per transmitted byte.
+/*/
+void transmitBitStartStop()
+{
+  delayMicroseconds(charDelay);
+  if(incoming == 0)
+  {
+    return;
+  }
+
+  if(newTransmission)
+  {
+    transmittedBits = 0;
+  }
+
+  if (bitRead(incoming, transmittedBits) == 1)
+  {
+    Timer1.setPeriod(highFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, true);
+
+    doDelay();
+  }
+  else
+  {
+    Timer1.setPeriod(lowFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, false);
+
+    doDelay();
+  }
+
+  /*if(digitalRead(outputTest) == true) Serial.write(1);
+  else Serial.write(0);//*/
+
+  if (delayEnable)
+  {
+    digitalWrite(led, HIGH);
+    doDelay();
+    digitalWrite(led, LOW);
+    doDelay();
+  }
+
+  if (transmittedBits < 7)
+  {
+    newTransmission = false;
+    transmittedBits++;
+  }
+  else
+  {
+    //Serial.write(incoming);
+    transmittedBits = 0;
+    incoming = 0;
+
+    Timer1.setPeriod(lowFreq);
+    Timer1.setPwmDuty(transmitterOut, 512);
+
+    digitalWrite(outputTest, false);
+  }
+}
+
+/*/
+ *  For each byte, receives the extra 'start' bit and then receives eight data bits to then reconstruct into a data byte from the transmitter.
+ *  While the 'start' bit has not been found, the receiver is always checking for start bits - if 0 is received instead, repeat.
+/*/
+void receiveBitExtraBit()
 {
   uint8_t votes = 5, carry;
   float holder = 0;
@@ -196,9 +273,9 @@ void receiveBit()
 
   if(outgoing == comma && !newReception)
   {
+    newReception = true;
     receivedBits = 0;
     outgoing = 0;
-    newReception = true;
   }
   else if(newReception)
   {
@@ -210,9 +287,115 @@ void receiveBit()
         Serial.write(outgoing);
       }
 
+      newReception = false;
       receivedBits = 0;
       outgoing = 0;
-      newReception = false;
+    }    
+  }
+}
+
+/*
+ *  While a message is not being received, wait for start byte to be read.
+ *  Once start byte is received, synchronize eight bit reconstruction in a stream.
+ *  Once stop byte is received, go back to waiting for start byte.
+ */
+void receiveBitStartStop()
+{
+  uint8_t votes = 5, carry;
+  float holder = 0;
+
+  for(int i=0; i<votes; i++)
+  {
+    if (analogRead(receiverIn) > 512)
+    {
+      holder += 1;
+    }
+    else
+    {
+      holder += 0;
+    }
+  }
+
+  holder /= votes;
+
+  if(!newReception)
+  {
+    if(holder > 0.5)
+    {
+      carry = 1;
+      outgoing <<= 1;
+      outgoing += carry;
+    }
+    else
+    {
+      carry = 0;
+      outgoing <<= 1;
+      outgoing += carry;
+    } 
+  }
+  else
+  {
+    if(holder > 0.5)
+    {
+      carry = 1;
+      //if(random(0,99) < errorChance) carry = 0;
+      outgoing |= carry << (receivedBits - 1);
+    }
+    else
+    {
+      carry = 0;
+      //if(random(0,99) < errorChance) carry = 1;
+      outgoing |= carry << (receivedBits - 1);
+    }
+  }
+
+  uint8_t h1 = 0, h2 = 0;
+  bool corruptFlag = false;
+
+  if(!(outgoing == startMessage && outgoing == endMessage && outgoing == startChunk && outgoing == endChunk))
+  {
+    for(int i = 0; i < 8; i++)
+    {
+      if(random(0,99) < errorChance) h1 = 0;
+      h2 |= (bitRead(outgoing, i) ^ h1) << i;
+    }
+    corruptFlag = true;
+  }
+
+  if(outgoing == startMessage && !newReception)
+  {
+    Serial.write(outgoing);
+    newReception = true;
+    receivedBits = 0;
+    outgoing = 0;
+  }
+  else if(newReception)
+  {
+    if (receivedBits < 7) receivedBits++;
+    else
+    {
+      if(corruptFlag)
+      {
+        if(h2 != 0)
+        {
+          Serial.write(h2);
+        }
+      }
+      else
+      {
+        if(outgoing != 0)
+        {
+          Serial.write(outgoing);
+        }
+  
+        if(outgoing == endMessage)
+        {
+          newReception = false;
+        }
+      }
+
+      receivedBits = 0;
+      outgoing = 0;
     }    
   }
 }
